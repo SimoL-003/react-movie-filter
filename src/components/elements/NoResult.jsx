02db5aad479f6751d724f5ab89458ed382d0660a
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-11-12 21:19:50 +0100

    feat: show "No Result" message if no films match filter parameters

---
+++ src/components/elements/NoResult.jsx
@@ -0,0 +1,7 @@
+export default function NoResult() {
+  return (
+    <div className="px-5 py-3 border-2 border-red-600 bg-red-200 rounded-md w-fit">
+      <p className="font-semibold">Nessun risultato trovato</p>
+    </div>
+  );
+}
